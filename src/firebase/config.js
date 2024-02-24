3a931d2233a277e8f6db986bb743be363e659671
Rodolpho Reis <rodolphoreis@live.com>
2024-02-24 20:49:42 +0000

    feat: install firebase and config

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQVcyhLRMMlgkSCMRjJOKWOATcHYsgeUQ",
+  authDomain: "blog-d2cc0.firebaseapp.com",
+  projectId: "blog-d2cc0",
+  storageBucket: "blog-d2cc0.appspot.com",
+  messagingSenderId: "689477733213",
+  appId: "1:689477733213:web:13b27201234be78f41f6b4",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
