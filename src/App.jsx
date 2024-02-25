3f97855c7a1268002a8a2baa0327655b09f643da
Rodolpho Reis <rodolphoreis@live.com>
2024-02-25 09:42:47 +0000

    feat: include navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import "./App.css";
 // react router dom imports
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// components imports
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
 //pages
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -12,12 +16,14 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <NavBar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
