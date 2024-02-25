4166060c040eb1bb8700289ad3d0b5c80a2fddfb
Rodolpho Reis <rodolphoreis@live.com>
2024-02-25 19:46:54 +0000

    feat: add button menu mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,88 @@
-// css
+// CSS
 import styles from "./NavBar.module.css";
 
-// react router dom imports
+// React imports
+import { useState, useEffect } from "react";
+import classnames from "classnames";
+
+// React router dom imports
 import { NavLink } from "react-router-dom";
 
+// Fontawesome library
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
+// AOS library
+import "aos/dist/aos.css";
+
 const NavBar = () => {
+  const [isMobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuVisible(!isMobileMenuVisible);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuVisible(false);
+  };
+
   return (
-    <nav className={styles.navBar}>
-      <NavLink to="/" className={styles.brand}>
-        DEV <span>Blog</span>
-      </NavLink>
-      <ul className={styles.links_list}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Sobre
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navBar}>
+      <div className={styles.nav}>
+        <NavLink to="/" className={styles.brand}>
+          DEV <span>Blog</span>
+        </NavLink>
+        <ul
+          className={`${styles.linksList} ${
+            isMobileMenuVisible ? styles.active : ""
+          }`}
+        >
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeMobileMenu}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/login"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeMobileMenu}
+            >
+              Entrar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/register"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeMobileMenu}
+            >
+              Cadastrar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? styles.active : "")}
+              onClick={closeMobileMenu}
+            >
+              Sobre
+            </NavLink>
+          </li>
+        </ul>
+        <div className={styles.mobileMenuButton} onClick={toggleMobileMenu}>
+          {isMobileMenuVisible ? (
+            <FontAwesomeIcon icon={faTimes} />
+          ) : (
+            <FontAwesomeIcon icon={faBars} />
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
