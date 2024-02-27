c5d1fec60e7354778a1d941093cff335c2bf3f33
Rodolpho Reis <rodolphoreis@live.com>
2024-02-27 17:26:29 +0000

    feat: Add custom hook for Firebase authentication

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -25,4 +25,33 @@ export const useAuthentication = () => {
       return;
     }
   }
+  const createUser = async (data) => {
+    checkifIsCancelled();
+    setLoading(true);
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
 };
