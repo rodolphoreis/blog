3bd50327b052cd4b261b2c29ea6bd7a37f8bcd8a
Rodolpho Reis <rodolphoreis@live.com>
2024-02-28 16:21:55 +0000

    feat: Add error handling in the catch block

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -1,3 +1,4 @@
+import { db } from "../firebase/config";
 // Firebase imports
 import {
   getAuth,
@@ -28,6 +29,7 @@ export const useAuthentication = () => {
   const createUser = async (data) => {
     checkifIsCancelled();
     setLoading(true);
+    setError(null);
     try {
       const { user } = await createUserWithEmailAndPassword(
         auth,
@@ -40,6 +42,18 @@ export const useAuthentication = () => {
     } catch (error) {
       console.log(error.message);
       console.log(typeof error.message);
+
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "E-mail já cadastrado.";
+        co;
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde.";
+      }
+      setError(systemErrorMessage);
     }
     setLoading(false);
   };
