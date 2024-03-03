8bd7279f5f2dc672c0f677aaf2937418eb3a25aa
Rodolpho Reis <rodolphoreis@live.com>
2024-03-03 19:40:59 +0000

    feat: create about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,22 @@
 //css
 import styles from "./About.module.css";
 
+// react router dom imports
+import { Link } from "react-router-dom";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Dev <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um Blog feito com React no front-end e Firebase
+        no back-end.
+      </p>
+      <Link to="/posts/create" className="btn">
+        Criar post
+      </Link>
     </div>
   );
 };
