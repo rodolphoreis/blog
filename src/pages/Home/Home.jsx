c4d0d5110008dcb5674bc6d3eac1b423cd437673
Rodolpho Reis <rodolphoreis@live.com>
2024-04-06 21:09:31 +0100

    feat: create state

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
 
 const Home = () => {
+  const [query, setQuery] = useState("");
   const [posts] = useState([]);
   return (
     <div>
