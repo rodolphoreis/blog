981e9ed7abeb39df6ae7513286b5507b9758a90e
Rodolpho Reis <rodolphoreis@live.com>
2024-04-06 21:10:25 +0100

    feat: create form Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,6 +16,27 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h1>Posts...</h1>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
