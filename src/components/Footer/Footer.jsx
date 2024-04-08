bb6bee696533ef7faa3d6fa7b47e4cd9065a8e98
Rodolpho Reis <rodolphoreis@live.com>
2024-04-08 16:55:30 +0100

    refactor: add className with new styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,10 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Escreva o que quiser sobre programação!</h3>
-      <p>DEV Blog - Rodolpho Reis &copy; 2024</p>
+      <h3 className={styles.footer_h3}>
+        Escreva o que quiser sobre programação!
+      </h3>
+      <p className={styles.footer_p}>DEV Blog - Rodolpho Reis &copy; 2024</p>
     </footer>
   );
 };
