c0b1f26247f5a3a3f744894d5c1ff54f2baea557
Rodolpho Reis <rodolphoreis@live.com>
2024-05-16 17:28:10 +0100

    feat: add link

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,6 +1,7 @@
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
 import PostDetail from "../../hooks/postDetail";
+import { Link } from "react-router-dom";
 
 const Search = () => {
   const query = useQuery();
@@ -12,6 +13,14 @@ const Search = () => {
     <div>
       <p> {search}</p>
       <div>
+        {posts && posts.length === 0 && (
+          <>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Criar primeiro post
+            </Link>
+          </>
+        )}
         {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
     </div>
