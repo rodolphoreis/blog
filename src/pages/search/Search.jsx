4af5b1348b1af3a7aa7b286c8ec241f8a2abd9d9
Rodolpho Reis <rodolphoreis@live.com>
2024-05-16 17:22:11 +0100

    feat: add PostDetail

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,13 +1,19 @@
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useQuery } from "../../hooks/useQuery";
+import PostDetail from "../../hooks/postDetail";
 
 const Search = () => {
   const query = useQuery();
   const search = query.get("q");
 
+  const { documents: posts } = useFetchDocuments("posts");
+
   return (
     <div>
       <p> {search}</p>
+      <div>
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   );
 };
